31142686e3f26fed3735387659f4fa85d0f52cef
Clint Milner <clinton.milner@gmail.com>
2018-04-30 22:25:07 +0100

    named exports syntax

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
-console.log('app.js loaded');
+import { init, initUtils } from './rs-utils.js';
+
+
+
+console.log(`here is app.js and ${init('init!')}`);
+console.info(`here is app.js and ${initUtils('awesome!')}`);
---
+++ src/rs-utils.js
@@ -0,0 +1,6 @@
+console.log('rs-utils is running');
+
+const init = (msg) => `rs-utils is ${msg}`;
+export const initUtils = (msg) => `rs-utils is ${msg}`;
+
+export { init };
