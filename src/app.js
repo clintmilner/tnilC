d675b6ade08bb24994692a365b25eb58a8dc199a
Clint Milner <clinton.milner@gmail.com>
2018-05-07 21:53:09 +0100

    good starting point for new projects

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { init, initUtils } from './rs-utils.js';
+import './styles/styles.scss';
 
+const tmpl = <p>React build from JSX and Babel</p>;
+ReactDOM.render( tmpl, document.getElementById('container'));
 
-
-console.log(`here is app.js and ${init('init!')}`);
+console.warn(`here is app.js and ${init('init!')}`);
 console.info(`here is app.js and ${initUtils('awesome!')}`);
